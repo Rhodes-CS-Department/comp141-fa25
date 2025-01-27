cae6e8f936c1eb8b30a7f26f8248bbd617597fef
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2025-01-27 08:29:47 -0600

    Create daily_quizzes.py

---
+++ king/daily_quizzes.py
@@ -0,0 +1,10 @@
+# Quiz, Jan 27
+user_input = input("Give me a number between 1 and 100: ") # just make up a number, assume it is an integer
+print("You gave:", user_input)
+print("Now watch this:")
+tens = int(user_input) // 10
+ones = int(user_input) % 10
+print("There are", tens, "tens and", ones, "ones in", user_input)
+ones = tens
+tens = ones
+print("Now we have", tens, "and", ones)
