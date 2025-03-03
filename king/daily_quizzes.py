f4ea3735b360ca7424c4f90eae876a785bdf0e07
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2025-03-03 08:12:29 -0600

    Update daily_quizzes.py

--- king/daily_quizzes.py
+++ king/daily_quizzes.py
@@ -110,7 +110,7 @@ def main():
 
 main()
 
-# Quiz 8, Feb 21, 2024
+# Quiz 8, Feb 21, 2025
 def helper1(x):
   if x < 12:
     return 10
@@ -135,3 +135,19 @@ def main():
   print(iterations)
 
 main()
+
+# Quiz 9, Mar 3, 2025
+def get_valid_input():
+  valid = False
+  while not valid:
+    response = input("Say a color!")
+    if response == "red" or response == "green" or response == "blue":
+      valid = True
+  return response
+
+def spell_color():
+   color = get_valid_input()
+   for letter in color: # this is new! can you guess what it does?
+     print(letter, end = "!")
+
+spell_color()
